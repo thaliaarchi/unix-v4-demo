1865564d952047c0dc648920f3169b2bcd64c5ad
Thalia Archibald <thalia@archibald.dev>
2026-08-29 00:55:05 +0100

    Implement zfree

--- fs/usr/thalia/brktest.c
+++
@@ -1,9 +0,0 @@
-main()
-{
-	char *a, *b, *c;
-	a = sbrk(0);
-	b = sbrk(1);
-	c = sbrk(1);
-	printf("%d\n", b-a);
-	printf("%d\n", c-a);
-}
--- fs/usr/thalia/ws/z.c
+++ fs/usr/thalia/ws/z.c
@@ -9,7 +9,7 @@
 #define NULL 0
 /* The multiple of bytes to allocate at once.
  * Should be divisible by 64. */
-#define BLKSIZ 512
+#define NALLOC 512
 
 /* Header for an arbitrary-precision integer.
  * The limbs directly follow it, inline. */
@@ -17,21 +17,25 @@ struct zhdr {
 	/* The absolute value is the number of limbs representing this number.
 	 * If negative, this is a negative number. */
 	int z_len;
-	/* The number of allocated limbs. */
-	int z_cap;
+	/* The number of allocated limbs. Keep at the same offset as f_alloc. */
+	int z_alloc;
 	/* The count of references to this number. */
 	int z_refs;
 };
+/* The size of zhdr. */
+#define ZSIZE 6
 
 struct freehdr {
-	struct freehdr *f_next;	/* Next block on free list */
-	int	f_size;		/* Size of this block */
+	/* Next block on free list. */
+	struct freehdr *f_next;
+	/* Size of this block. Keep at the same offset as z_alloc. */
+	int	f_alloc;
 };
 
 struct freehdr *base { base, 0 };	/* Initial empty list */
 struct freehdr *freep base;
 
-struct zhdr *zgrow();
+struct freehdr *zgrow();
 
 struct zhdr *
 zalloc(limbs)
@@ -39,15 +43,15 @@ zalloc(limbs)
 	register nbytes;
 	register char *p, *prevp;
 
-	nbytes = sizeof *p + limbs * 2;
+	nbytes = ZSIZE + limbs * 2;
 	for (prevp = p = freep; ; prevp = p, p = p->f_next) {
-		if (p->f_size >= nbytes) {	/* Large enough */
-			if (p->f_size == nbytes)	/* Fits exactly */
+		if (p->f_alloc >= nbytes) {	/* Large enough */
+			if (p->f_alloc == nbytes)	/* Fits exactly */
 				prevp->f_next = p->f_next;
 			else {	/* Allocate tail */
-				p->f_size =- nbytes;
-				p =+ p->f_size;
-				p->f_size = nbytes;
+				p->f_alloc =- nbytes;
+				p =+ p->f_alloc;
+				p->f_alloc = nbytes;
 			}
 			freep = prevp;
 			return p;
@@ -55,16 +59,49 @@ zalloc(limbs)
 		/* Wrapped around the free list */
 		if (p == freep) {
 			p = zgrow(nbytes);
-			if (p == NULL) {
-				write(2, "ws: Out of memory\n", 18);
-				exit(1);
-			}
 		}
-
 	}
 }
 
-struct zhdr *
+struct freehdr *
 zgrow(nbytes)
 {
+	register struct freehdr *p;
+
+	if (nbytes < NALLOC)
+		nbytes = NALLOC;
+	p = sbrk(nbytes);
+	if (p == -1) {
+		write(2, "ws: Out of memory\n", 18);
+		exit(1);
+	}
+	p->f_alloc = nbytes;
+	zfree(p);
+	return freep;
+}
+
+zfree(z)
+struct freehdr *z;
+{
+	register char *zp, *fp;
+
+	zp = z;
+	for (fp = freep; ; fp = fp->f_next) {
+		if (fp < zp && zp < fp->f_next)
+			break;	/* Between free blocks */
+		if (fp >= fp->f_next && (fp < zp || zp < fp->f_next))
+			break;	/* At start or end of arena */
+	}
+
+	if (zp + zp->f_alloc == fp->f_next) {	/* Join above */
+		zp->f_alloc =+ fp->f_next->f_alloc;
+		zp->f_next = fp->f_next->f_next;
+	} else
+		zp->f_next = fp->f_next;
+	if (fp + fp->f_alloc == zp) {	/* Join below */
+		fp->f_alloc =+ zp->f_alloc;
+		fp->f_next = zp->f_next; /* zp->f_next already assigned */
+	} else
+		fp->f_next = zp;
+	freep = fp;
 }
