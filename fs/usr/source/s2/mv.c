928c3e665c1dbdffdc5c17632606f601180b6071
Thalia Archibald <thalia@archibald.dev>
2026-09-07 18:44:35 -0600

    Print to stderr and exit with 1 for mv errors

--- fs/usr/source/s2/mv.c
+++ fs/usr/source/s2/mv.c
@@ -38,8 +38,8 @@ char *argv[];
 		of arguments
 	*/
 	if(argc != 3) {
-		write(1,"Usage: mv name1 name2\n",22);
-		exit();
+		write(2,"Usage: mv name1 name2\n",22);
+		exit(1);
 	}
 	/*
 		is there anything to do?
@@ -47,8 +47,8 @@ char *argv[];
 	argp3 = argp[1];
 	argp4 = argp[2];
 	if(stat(argp[1], stbuf) < 0) {
-		write(1,"Source file non-existent\n",25);
-		exit();
+		write(2,"Source file non-existent\n",25);
+		exit(1);
 	}
 	/*
 		yes, there is a source.
@@ -65,31 +65,31 @@ char *argv[];
 		restrictions.
 	*/
 		if(stat(argp[2], stbuf) >= 0) {
-			write(1,"Directory target exists.\n",25);
-			exit();
+			write(2,"Directory target exists.\n",25);
+			exit(1);
 		}
 		argp1 = argp[1];
 		argp2 = argp[2];
 		while(*argp1 == *argp2) {
 			argp1++;
 			if(*argp2++ == 0) {
-				write(1,"???\n",4);
-				exit();
+				write(2,"???\n",4);
+				exit(1);
 			}
 		}
 		while(*argp1)
 			if(*argp1++ == '/') {
-				write(1,"Directory rename only\n",22);
-				exit();
+				write(2,"Directory rename only\n",22);
+				exit(1);
 			}
 		while(*argp2)
 			if(*argp2++ == '/') {
-				write(1,"Directory rename only\n",22);
-				exit();
+				write(2,"Directory rename only\n",22);
+				exit(1);
 			}
 		if(*--argp1 == '.') {
-			write(1,"values of B will give rise to dom!\n",37);
-			exit();
+			write(2,"values of B will give rise to dom!\n",37);
+			exit(1);
 		}
 	} else {
 	/*
@@ -111,13 +111,14 @@ char *argv[];
 			}
 			if(stat(argp4, &stbuf[2]) >= 0) {
 				if((stbuf[0]==stbuf[2]) && (stbuf[1]==stbuf[3])) {
-					write(1,"Files are identical.\n",21);
-					exit();
+					write(2,"Files are identical.\n",21);
+					exit(1);
 				}
 				if(getuid() == stbuf[2].uid)
-					b = 0200; else
-				if(getgid() == stbuf[2].gid)
-					b = 020; else
+					b = 0200;
+				else if(getgid() == stbuf[2].gid)
+					b = 020;
+				else
 					b = 02;
 				if((stbuf[2].imode & b) == 0) {
 					printf("%s: %o mode ", argp4,
@@ -126,11 +127,11 @@ char *argv[];
 					while(b != '\n' && b != '\0')
 						b = getchar();
 					if(i != 'y')
-						exit();
+						exit(1);
 				}
 				if(unlink(argp4) < 0) {
-					write(1,"Cannot remove target file.\n",27);
-					exit();
+					write(2,"Cannot remove target file.\n",27);
+					exit(1);
 				}
 			}
 		}
@@ -138,8 +139,8 @@ char *argv[];
 	if(link(argp3, argp4) < 0) {
 		i = fork();
 		if(i == -1) {
-			write(1,"Try again.\n",11);
-			exit();
+			write(2,"Try again.\n",11);
+			exit(1);
 		}
 		if(i) {
 			while(wait(&status) != i);
@@ -150,19 +151,19 @@ char *argv[];
 			p2 = p;
 			while(*p++ = *argp4++);
 			execl("/bin/cp","cp", p1, p2, 0);
-			write(1, "no cp\n", 6);
+			write(2, "no cp\n", 6);
 			exit(1);
 		}
 		if((status & 0377) != 0) {
-			write(1,"?\n", 2);
-			exit();
+			write(2,"?\n", 2);
+			exit(1);
 		}
 		if(status != 0)
-			exit();
+			exit(1);
 	}
 	if(unlink(argp3) < 0) {
-		write(1,"Cannot unlink source file.\n",26);
-		exit();
+		write(2,"Cannot unlink source file.\n",26);
+		exit(1);
 	}
 }
 
