b96944a73bb30985d782cc8d0cabdf363f409e92
Thalia Archibald <thalia@archibald.dev>
2026-09-10 08:25:58 -0600

    Fix stat.h comments

--- fs/usr/sys/stat.h
+++ fs/usr/sys/stat.h
@@ -5,8 +5,8 @@ struct stat {
 	char	st_nlink;	/* number of links to file */
 	char	st_uid;		/* user ID of owner */
 	char	st_gid;		/* group ID of owner */
-	char	st_siz0;	/* high byte of 23-bit size */
-	int	st_siz;	/* low word of 24-bit size */
+	char	st_siz0;	/* high byte of 24-bit size */
+	int	st_siz;		/* low word of 24-bit size */
 	int	st_addr[8];	/* block numbers or device number */
 	/*unsigned*/int	st_atime[2];	/* time of last access */
 	/*unsigned*/int	st_mtime[2];	/* time of last modification */
